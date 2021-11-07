8d017bb6f5a5ee96aa5c768f7e259f99dcda999c
tocgic <jeongyon.cho@gmail.com>
2021-11-07 17:22:28 +0900

    6. Creating columns and rows - Column 을 사용하여, 두 개의 텍스트 요소가 있는 열을 표시
    - Column : LinearLayout 의 vertical orientation 효과
    - Row : LinearLayout 의 Horizontal orientation 효과
    - Box : FrameLayout 효과

--- week 1-Jetpack Compose basics/MyBasicCodelab/app/src/main/java/com/tocgic/sample/mybasicscodelab/MainActivity.kt
+++ week 1-Jetpack Compose basics/MyBasicCodelab/app/src/main/java/com/tocgic/sample/mybasicscodelab/MainActivity.kt
@@ -3,6 +3,7 @@ package com.tocgic.sample.mybasicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -35,7 +36,10 @@ private fun MyApp() {
 @Composable
 fun Greeting(name: String) {
     Surface(color = MaterialTheme.colors.primary) {
-        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+        Column (modifier = Modifier.padding(24.dp)) {
+            Text(text = "Hello,")
+            Text(text = "$name!")
+        }
     }
 }
 
