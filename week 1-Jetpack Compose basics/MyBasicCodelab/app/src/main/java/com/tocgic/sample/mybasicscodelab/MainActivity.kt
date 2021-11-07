4b774c7ca132a3c9965e38fb3f9eac74d052e68d
tocgic <jeongyon.cho@gmail.com>
2021-11-08 00:47:58 +0900

    코드 정리 - by 사용

--- week 1-Jetpack Compose basics/MyBasicCodelab/app/src/main/java/com/tocgic/sample/mybasicscodelab/MainActivity.kt
+++ week 1-Jetpack Compose basics/MyBasicCodelab/app/src/main/java/com/tocgic/sample/mybasicscodelab/MainActivity.kt
@@ -92,10 +92,10 @@ private fun Greetings(names: List<String> = List(1000) { "$it"}) {
 
 @Composable
 fun Greeting(name: String) {
-    val expanded = remember { mutableStateOf(false) }
+    var expanded by remember { mutableStateOf(false) }
 
     val extraPadding by animateDpAsState(
-        if (expanded.value) 48.dp else 0.dp,
+        if (expanded) 48.dp else 0.dp,
         animationSpec = spring(
             dampingRatio = Spring.DampingRatioMediumBouncy,
             stiffness = Spring.StiffnessLow
@@ -120,9 +120,9 @@ fun Greeting(name: String) {
                 )
             }
             OutlinedButton(
-                onClick = { expanded.value = !expanded.value }
+                onClick = { expanded = !expanded }
             ) {
-                Text(if (expanded.value) "Show less" else "Show more")
+                Text(if (expanded) "Show less" else "Show more")
             }
         }
     }
