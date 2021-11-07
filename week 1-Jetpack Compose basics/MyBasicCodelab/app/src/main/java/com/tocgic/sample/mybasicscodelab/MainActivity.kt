3dc1bbdf7eccca1b2b65f2ecd551e7906a277cbe
tocgic <jeongyon.cho@gmail.com>
2021-11-07 16:08:01 +0900

    2. Starting a new Compose project

---
+++ week 1-Jetpack Compose basics/MyBasicCodelab/app/src/main/java/com/tocgic/sample/mybasicscodelab/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.tocgic.sample.mybasicscodelab
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.tocgic.sample.mybasicscodelab.ui.theme.MyBasicsCodelabTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyBasicsCodelabTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyBasicsCodelabTheme {
+        Greeting("Android")
+    }
+}
