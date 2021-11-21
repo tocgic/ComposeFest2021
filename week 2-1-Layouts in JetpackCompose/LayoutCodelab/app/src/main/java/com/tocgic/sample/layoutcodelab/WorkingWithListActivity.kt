77d3f0d92c267ab0baafd9893096ba6b8f67b22f
tocgic <jeongyon.cho@gmail.com>
2021-11-22 00:16:51 +0900

    week2-1. 6. Displaying images - Image 처리

--- week 2-1-Layouts in JetpackCompose/LayoutCodelab/app/src/main/java/com/tocgic/sample/layoutcodelab/WorkingWithListActivity.kt
+++ week 2-1-Layouts in JetpackCompose/LayoutCodelab/app/src/main/java/com/tocgic/sample/layoutcodelab/WorkingWithListActivity.kt
@@ -3,14 +3,22 @@ package com.tocgic.sample.layoutcodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.tocgic.sample.layoutcodelab.ui.theme.LayoutCodelabTheme
 
 class WorkingWithListActivity : ComponentActivity() {
@@ -20,7 +28,8 @@ class WorkingWithListActivity : ComponentActivity() {
             LayoutCodelabTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    SimpleList()
+//                    SimpleList()
+                    ImageList(Modifier)
                 }
             }
         }
@@ -40,10 +49,37 @@ fun SimpleList() {
     }
 }
 
+@Composable
+fun ImageListItem(index: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = rememberImagePainter(
+                data = "https://developer.android.com/images/brand/Android_Robot.png"
+            ),
+            contentDescription = "Android Logo",
+            modifier = Modifier.size(50.dp)
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Text(text = "Item #$index", style = MaterialTheme.typography.subtitle1)
+    }
+}
+
+@Composable
+fun ImageList(modifier: Modifier = Modifier) {
+    val scrollState = rememberLazyListState()
+
+    LazyColumn(state = scrollState) {
+        items(100) {
+            ImageListItem(index = it)
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun SimpleListPreview() {
     LayoutCodelabTheme {
-        SimpleList()
+//        SimpleList()
+        ImageList(Modifier)
     }
 }
