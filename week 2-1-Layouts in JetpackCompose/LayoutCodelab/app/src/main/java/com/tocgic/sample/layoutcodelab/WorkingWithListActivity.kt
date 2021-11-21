1679cbbbe657f985c6653edcc789a8e6f714b59c
tocgic <jeongyon.cho@gmail.com>
2021-11-21 23:57:08 +0900

    week2-1. 6. Working with lists - Lazy List : LazyColumn, rememberLazyListState() 추가
    - 화면에 보이는 항목만 렌더링하는 LazyColumn을 사용하여 성능 향상을 허용
    - 수정자를 스크롤할 필요가 없음

--- week 2-1-Layouts in JetpackCompose/LayoutCodelab/app/src/main/java/com/tocgic/sample/layoutcodelab/WorkingWithListActivity.kt
+++ week 2-1-Layouts in JetpackCompose/LayoutCodelab/app/src/main/java/com/tocgic/sample/layoutcodelab/WorkingWithListActivity.kt
@@ -3,14 +3,13 @@ package com.tocgic.sample.layoutcodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.tocgic.sample.layoutcodelab.ui.theme.LayoutCodelabTheme
 
@@ -32,10 +31,10 @@ class WorkingWithListActivity : ComponentActivity() {
 fun SimpleList() {
     // 스크롤 위치를 이 상태로 저장합니다.
     // 프로그래밍 방식으로 목록을 스크롤하는 데 사용
-    val scrollState = rememberScrollState()
+    val scrollState = rememberLazyListState()
 
-    Column(modifier = Modifier.verticalScroll(scrollState)) {
-        repeat(100) {
+    LazyColumn(state = scrollState) {
+        items(100) {
             Text(text = "Item #$it")
         }
     }
